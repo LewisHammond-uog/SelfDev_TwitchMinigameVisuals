02fcd32cb6e0d5c9064d6b0d8e9e054f2c85ea7f
Lewis Hammond <s1800707@glos.ac.uk>
2020-08-13 16:50:56 +0100

    Remove Secret Details

--- Assets/Scripts/Twitch/TwitchAPIInfo.cs
+++ Assets/Scripts/Twitch/TwitchAPIInfo.cs
@@ -11,8 +11,8 @@ public static class TwitchAuthInfo
     public static string botName = "lewish_unity";
 
     //Secret info for connecting to twitch
-    public static string client_id = "kv3dpei0jwikntf7qhzl3wt0bfpgqs";
-    public static string client_secret = "wampndgfg2wdeozy239qxujhkvc9yu";
-    public static string bot_access_token = "o9hmepxei6gy776yqfbpcy7gb91npp";
-    public static string bot_refresh_token = "5rohzkjqlm3mierplon0hnp75e4w3ugtr39tc5bt6i0cir414x";
+    public static string client_id = "";
+    public static string client_secret = "";
+    public static string bot_access_token = "";
+    public static string bot_refresh_token = "";
 }
