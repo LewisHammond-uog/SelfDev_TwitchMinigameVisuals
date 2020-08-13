e31b4a45838498a2d6c536576e6cbf5c42972c53
Lewis Hammond <s1800707@glos.ac.uk>
2020-08-13 16:43:10 +0100

    Command Based Movement + Attacking

---
+++ Assets/Scripts/Games/BR/BRAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//simple attack
+public class BRAttack : MonoBehaviour
+{
+    [SerializeField]
+    private float attackTime = 3f;//Attack Time (in seconds)
+
+    [SerializeField]
+    private float attackDamage = 100f;
+
+    //Raw gameobject that is excempt from damage
+    private GameObject creatingObject;
+    //Property for creator, makes sure we can only set a player
+    public BRPlayer Creator {
+        get { return creatingObject.GetComponent<BRPlayer>(); }
+        set { creatingObject = (creatingObject == null ? value.gameObject : creatingObject); } //Only allow change if a creator is not set or was deleted
+    }
+
+    private void Start()
+    {
+        Destroy(this.gameObject, attackTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Don't damage our creator
+        if(other.gameObject == creatingObject)
+        {
+            return;
+        }
+
+
+        //Only try to damage players
+        BRPlayer player;
+        if(other.TryGetComponent(out player))
+        {
+            player.Damage(attackDamage);
+        }
+    }
+}
--- Assets/Scripts/Games/BR/BRPlayer.cs
+++ Assets/Scripts/Games/BR/BRPlayer.cs
@@ -2,31 +2,138 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class BRPlayer : MonoBehaviour
 {
 
-    private string playerName;
-    public string PlayerName {
-        set { playerName = playerName == string.Empty ? value : playerName ; }
-        get { return playerName; } 
-    }
+    //NavMesh Agent
+    [SerializeField]
+    private NavMeshAgent navigator;
+
+    [HideInInspector]
+    public string playerName; //todo fix this
     public Text uiText;
+
+    [SerializeField]
+    private float commandMoveAmount = 25f; //Amount to move when a command is entered
+
+    private Vector3 targetPos;
+
+    //Timeouts for commands (i.e the time you have to wait before sending the next command)
+    [SerializeField]
+    private float moveTO, lightAttackTO = 3f;
+
+    //Prefab for player attack
+    [SerializeField]
+    private GameObject attackPrefab;
+
+    private float health = 100f;
+
+    //Attack Commands
+    private string attackCommand = "attack";
+
+    //List of commands and directions that they shoud go
+    //Command, Direction
+    private Dictionary<string, Vector3> commandDirectionPairs = new Dictionary<string, Vector3>()
+    {
+        { "left", Vector3.left },
+        { "right", Vector3.right },
+        { "forward", Vector3.forward },
+        { "back", Vector3.back },
+        { "up", Vector3.forward },
+        { "down", Vector3.back }
+    };
+
+
+    private void Start()
+    {
+        //Set target pos to current pos
+        targetPos = transform.position;
+        navigator.SetDestination(targetPos);
+    }
 
     private void Update()
     {
         uiText.text = playerName;
     }
 
-    private void ProcessMovement(object sender, TwitchLib.Client.Events.OnChatCommandReceivedArgs e)
+    /// <summary>
+    /// Process player chat commands
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ProcessPlayerCommands(object sender, TwitchLib.Client.Events.OnChatCommandReceivedArgs e)
     {
+        //If the commands are not for this player then about
+        if(e.Command.ChatMessage.Username != playerName)
+        {
+            return;
+        }
 
+
+        string commandText = e.Command.CommandText;
+        if (commandDirectionPairs.ContainsKey(commandText))
+        {
+            ProcessPlayerMovement(commandText);
+        }else if(commandText == attackCommand)
+        {
+            //Attack all players within range
+            CreateAttack();
+        }
     }
 
-    private void OnEnable() => TwitchChatClient.Instance.chatClient.OnChatCommandReceived += ProcessMovement;
+    /// <summary>
+    /// Does an amout of damage to the player
+    /// </summary>
+    /// <param name="damageAmount"></param>
+    public void Damage(float damageAmount)
+    {
+        health -= damageAmount;
+        Debug.Log("DAMAGED! For Debuggining I'm now going to delete you!");
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// Create an attack that damages all players apart from us
+    /// </summary>
+    private void CreateAttack()
+    {
+        GameObject attackGO = Instantiate(attackPrefab);
+        attackGO.transform.position = transform.position;
+
+        //Get the attack script to set the creator
+        BRAttack attack;
+        if (attackGO.TryGetComponent(out attack))
+        {
+            attack.Creator = this;
+        }
+    }
+
+    /// <summary>
+    /// Proccess player movement from a chat command
+    /// </summary>
+    /// <param name="movementCommandText"></param>
+    private void ProcessPlayerMovement(string movementCommandText)
+    {
+        //Set target to be the direction assosciated with the command * by the amount to move
+        targetPos += (commandDirectionPairs[movementCommandText] * commandMoveAmount);
+        navigator.SetDestination(targetPos);
+    }
+
+    private void OnEnable()
+    {
+        if (TwitchChatClient.Instance.chatClient != null)
+        {
+            TwitchChatClient.Instance.chatClient.OnChatCommandReceived += ProcessPlayerCommands;
+        }
+    }
 
     private void OnDisable()
     {
-        
+        if (TwitchChatClient.Instance.chatClient != null)
+        {
+            TwitchChatClient.Instance.chatClient.OnChatCommandReceived -= ProcessPlayerCommands;
+        }
     }
 }
